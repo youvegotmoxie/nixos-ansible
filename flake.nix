5a0f212a04f95f9f72d643cbe08804963da1f01d
Michael Beasley <michael.beasley@alvaria.com>
2025-07-22 12:48:10 -0800

    nevermind shit don't work on arm mac

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   # Use the default systems flake instead of defining a list of systems
   # [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
   inputs.systems.url = "github:nix-systems/default";
-  inputs.alejandra.url = "github:kamadorueda/alejandra/main";
-  inputs.alejandra.inputs.nixpkgs.follows = "nixpkgs";
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
     inputs.systems.follows = "systems";
@@ -14,7 +12,6 @@
   outputs = {
     nixpkgs,
     flake-utils,
-    alejandra,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -28,7 +25,7 @@
             nh
             sops
             ssh-to-age
-            alejandra.defaultPackage.${system}
+            alejandra
           ];
         };
       }
