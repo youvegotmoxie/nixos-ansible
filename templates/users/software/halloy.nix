7790daeb43de441720ed83745a894d96fcf1fe57
MikeB <youvegotmoxie@gmail.com>
2025-07-25 17:22:42 -0800

    don't configure halloy

--- templates/users/software/halloy.nix
+++ templates/users/software/halloy.nix
@@ -2,17 +2,5 @@
   # Install and configure Halloy
   programs.halloy = {
     enable = true;
-    settings = {
-      "buffer.channel.topic" = {
-        enabled = true;
-      };
-      "servers.liberachat" = {
-        channels = [
-          "#halloy"
-        ];
-        nickname = "moxie";
-        server = "irc.libera.chat";
-      };
-    };
   };
 }
