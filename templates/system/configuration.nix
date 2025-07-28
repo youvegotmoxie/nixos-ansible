e5c6fbc02ca4207ecb0cd13e3dd8de1942d0cab3
Michael Beasley <michael.beasley@alvaria.com>
2025-07-28 09:23:33 -0800

    systemd-boot: reduce bootloader entries to 3

--- templates/system/configuration.nix
+++ templates/system/configuration.nix
@@ -22,7 +22,10 @@
     tmp.useZram = true;
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 3;
+      };
       efi.canTouchEfiVariables = true;
       timeout = 2;
     };
